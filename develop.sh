f86247cea90ace391c52afa91272d3e3e473d500
Chris Matthias <chris.matthias@gmail.com>
2017-03-06 17:20:11 -0500

    updating the status message/title and adding some styles

--- develop.sh
+++ develop.sh
@@ -1,7 +1,7 @@
 SL9_CUSTOM_LOGO="//shorelinemedia.github.io/images/Shoreline-Media1.neuquant.png" \
 SL9_CLIENT_NAME="Shoreline Media Marketing" \
 SL9_PRIMARY_COLOR="#84C466" \
-SL9_UP_MESSAGE="<p>We are upgrading our website.  We'll be back online shortly.</p>" \
+SL9_UP_MESSAGE="<p>We apologize for the inconvenience. We'll be back online shortly. In the meantime, please call <a href='tel:888-591-8205'>888-591-8205</a> if you need immediate assistance.</p>" \
 SL9_BG_COLOR="#587E9D" \
 SL9_MAINTENANCE_MESSAGE="We are currently upgrading our website" \
 bundle exec jekyll serve --config _config.yml,_config_dev.yml --host 0.0.0.0 --force_polling
